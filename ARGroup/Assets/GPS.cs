f570d9e9d34301685432424dc7282866aa233197
lbk3 <40115865@live.napier.ac.uk>
2018-01-29 19:07:11 +0000

    Began AR work
    Began work on markerless AR scripts within the "Exlpore" scene

---
+++ ARGroup/Assets/GPS.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class GPS : MonoBehaviour
+{
+
+
+    public static GPS Instance { set; get; }
+    public float latitude;
+    public float longitude;
+
+    public GameObject objAR;
+    public Text coordinates;
+    public Text referenceCoordinates;
+    public Text distanceFrom3dModel;
+
+    private double referenceLatitude;
+    private double referenceLongitude;
+    private double referenceDistance;
+
+    private double distFrom3dModel_lat;
+    private double distFrom3dModel_lon;
+
+    private void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        objAR.SetActive(false);
+        referenceLatitude = 55.9287109;
+        referenceLongitude = -3.4948288;
+        referenceDistance = 0.00020;
+        distFrom3dModel_lat = 0;
+        distFrom3dModel_lon = 0;
+        distanceFrom3dModel.text = "Distance (Lan,Lon): 0 - 0 ";
+        referenceCoordinates.text = "Reference (Lat,Lon): " + referenceLatitude.ToString() + " - " + referenceLongitude.ToString();
+        StartCoroutine(StartLocationService());
+    }
+
+    private void Update()
+    {
+        latitude = Input.location.lastData.latitude;
+        longitude = Input.location.lastData.longitude;
+
+        coordinates.text = "Current (Lat,Lon): " + GPS.Instance.latitude.ToString() + " - " + GPS.Instance.longitude.ToString();
+        if (CloseEnoughForMe(Instance.latitude, referenceLatitude, referenceDistance) && CloseEnoughForMe(Instance.longitude, referenceLongitude, referenceDistance))
+        {
+            objAR.SetActive(true);
+        }
+        else
+        {
+            objAR.SetActive(false);
+        }
+    }
+
+    private bool CloseEnoughForMe(double value1, double value2, double acceptableDifference)
+    {
+        distanceFrom3dModel.text = "Distance (Lan,Lon): " + Math.Abs(value1 - value2).ToString();
+        return Math.Abs(value1 - value2) <= acceptableDifference;
+    }
+
+    private double distanceBetweenTwoPoints(double value1, double value2)
+    {
+        return Math.Abs(value1 - value2);
+    }
+
+    private IEnumerator StartLocationService()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("User has not enabled GPS");
+            yield break;
+        }
+
+        Input.location.Start();
+
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait <= 0)
+        {
+            Debug.Log("Timed out");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determin device location");
+            yield break;
+        }
+
+        latitude = Input.location.lastData.latitude;
+        longitude = Input.location.lastData.longitude;
+
+        yield break;
+    }
+}
