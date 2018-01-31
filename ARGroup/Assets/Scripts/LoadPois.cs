fa9dc95e6a6f50c989b06f8fac1b7b3bce33baf9
David Dryburgh <david.dryburgh1994@gmail.com>
2018-01-31 12:52:27 +0000

    Load Information
    An update has been made to the tours page. Now the page loads in a list
    of buttons with the name of each tour. When a user clicks a tour, a new
    page is loaded showing a description of the tour. Map interface still
    to be loaded into here and linking tour to AR still to be complete.
    The preferences page now loads in a list of points of interest. These
    are just text for now which doesn't do anything.

---
+++ ARGroup/Assets/Scripts/LoadPois.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadPois : MonoBehaviour {
+
+	public string[] items;
+	public Text poiText;
+	private string poiString;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		WWW itemsData = new WWW ("https://punier-boresights.000webhostapp.com/PoiData.php");
+		yield return itemsData;
+		string itemsDataString = itemsData.text;
+
+		items = itemsDataString.Split (';');
+		for (int i = 0; i < items.Length-1; i++) {
+			print (getDataValue (items [i], "Name:"));
+			poiString = poiString + getDataValue (items [i], "Name:") + "\n";
+		}
+
+		poiText.text = poiString;
+
+	}
+
+	string getDataValue(string data, string index){
+		string value = data.Substring (data.IndexOf(index)+index.Length);
+		if (value.Contains ("|")) {
+			value = value.Remove (value.IndexOf ("|"));
+		}
+		return value;
+	}
+}
