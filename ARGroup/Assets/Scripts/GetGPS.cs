c3a4f6b45562c0cb933008dcd7252a68ad3bdc20
David Dryburgh <david.dryburgh1994@gmail.com>
2018-02-28 20:37:31 +0000

    GetDeviceLocation
    Getting the device location based on Latitude and Longitude and
    displaying coordinates

---
+++ ARGroup/Assets/Scripts/GetGPS.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetGPS : MonoBehaviour {
+
+	public static GetGPS Instance { set; get; }
+
+	public float latitude;
+	public float longitude;
+
+	private void Start()
+	{
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+		StartCoroutine (StartLocationService());
+	}
+
+	private void Update(){
+		Instance = this;
+		StartCoroutine (UpdateLocation ());
+	}
+
+	private IEnumerator StartLocationService ()
+	{
+		//Make sure user enables GPS
+		if (!Input.location.isEnabledByUser) 
+		{
+			Debug.Log ("User has not enabled GPS");
+			yield break;
+		}
+
+		//Start GPS
+		Input.location.Start ();
+
+		int maxWait = 20;
+
+		//Wait for GPS to initialize
+		while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) 
+		{
+			yield return new WaitForSeconds (1);
+			maxWait--;
+		}
+
+		//Check for time out
+		if (maxWait <= 0) 
+		{
+			Debug.Log ("Timed out");
+			yield break;
+		}
+
+		if (Input.location.status == LocationServiceStatus.Failed) 
+		{
+			Debug.LogFormat ("Unable to determine device location");
+			yield break;
+		}
+
+		//get location from device
+		latitude = Input.location.lastData.latitude;
+		longitude = Input.location.lastData.longitude;
+
+		yield break;
+	}
+
+	private IEnumerator UpdateLocation(){
+		//get location from device
+		latitude = Input.location.lastData.latitude;
+		longitude = Input.location.lastData.longitude;
+
+		yield break;
+	}
+}
